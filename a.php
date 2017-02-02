f1ccc50b27b4b3cc8a74d16e540e224faddc45e3
James Titcumb <james@asgrim.com>
2017-02-02 14:02:24 -0500

    Added use of StorageCacheAbstractServiceFactory

--- a.php
+++ a.php
@@ -3,11 +3,30 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-$r = new Zend\Cache\Storage\Adapter\Redis([
-    'server' => [
-        'host' => 'localhost',
+use Zend\Cache\Service\StorageCacheAbstractServiceFactory;
+use Zend\Cache\Storage\Adapter\Redis;
+
+$config = [
+    'abstract_factories' => [
+        StorageCacheAbstractServiceFactory::class,
     ],
-    'password' => 'foobared',
-]);
+    'caches' => [
+        'mycache' => [
+            'adapter' => Redis::class,
+            'options' => [
+                'server' => [
+                    'host' => 'localhost',
+                ],
+                'password' => 'foobared',
+            ],
+        ],
+    ],
+];
+
+$sm = new \Zend\ServiceManager\ServiceManager($config);
+$sm->setService('config', $config); // to stop lol-recursion wtf
+
+$r = $sm->get('mycache');
 
 $r->addItem('mykey', uniqid('value', true));
+var_dump($r->getItem('mykey'));
